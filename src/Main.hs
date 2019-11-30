a08d04f9e21b498a9303125188e1eb64ff6ce433
DwarfMaster <luc@dwarfmaster.net>
2019-11-30 14:11:56 +1000

    Finish CLI arguments parsing

--- src/Main.hs
+++ src/Main.hs
@@ -22,7 +22,6 @@ data Action = List
             | Repo RepoAction
             | Activate
             | Deactivate
-            | Help
 instance Show Action where
     show List                = "List"
     show (Add wiki desc dir) = "Add " <> show desc <> " (" <> show dir <> ") " <> show wiki
@@ -32,7 +31,6 @@ instance Show Action where
     show (Repo ra)           = "Repo " <> show ra
     show Activate            = "Activate"
     show Deactivate          = "Deactivate"
-    show Help                = "Help"
 
 data Options = Options { project   :: Text   -- Might point anywhere on the project tree, the action
                                              -- will be executed on all sub projects (if the action
@@ -96,7 +94,6 @@ parseAction = subparser
            <> command "repos"      (info reposOptions  $ progDesc "Access the repos information")
            <> command "activate"   (info actOptions    $ progDesc "Mark project(s) as active")
            <> command "deactivate" (info deactOptions  $ progDesc "Mark project(s) as inactive")
-           <> command "help"       (info helpOptions   $ progDesc "Show help")
 
 listOptions :: Parser Action
 listOptions = pure List
@@ -113,22 +110,59 @@ removeOptions :: Parser Action
 removeOptions = pure Remove
 
 getOptions :: Parser Action
-getOptions = pure $ Get $ const "caca" -- TODO
+getOptions = subparser
+           $ command "name"        (info (fromGetter Pr.name)        $ progDesc "Get project name")
+          <> command "description" (info (fromGetter Pr.description) $ progDesc "Get project description")
+          <> command "directory"   (info (fromGetter Pr.directory)   $ progDesc "Get project directory")
+          <> command "wiki"        (info (fromGetter Pr.wiki)        $ progDesc "Get project wiki")
+          <> command "activated"   (info (fromGetter Pr.active)      $ progDesc "Is project active")
+ where fromGetter :: Show a => (Project -> a) -> Parser Action
+       fromGetter getter = pure $ Get $ show . getter
 
 setOptions :: Parser Action
-setOptions = pure $ Set id -- TODO
+setOptions = subparser
+           $ fromSetter "directory" (\dir p -> p { Pr.directory = dir })
+          <> fromSetter "wiki"      (\file p -> p { Pr.wiki = file })
+          <> command "description" (info descParser $ progDesc "Set project description")
+ where fromSetter :: Text -> (Text -> Project -> Project) -> Mod CommandFields Action
+       fromSetter name setter = let nm = toString name
+                                 in command nm (info (Set . setter <$> argument str (metavar nm))
+                                                   $ progDesc $ "Set project " ++ nm)
+       descParser :: Parser Action
+       descParser = Set . (\desc p -> p { Pr.description = desc }) . mconcat . intersperse " "
+                <$> many (argument str (metavar "description"))
 
 reposOptions :: Parser Action
-reposOptions = pure $ Repo RList -- TODO
+reposOptions = Repo <$> subparser
+             ( command "list"   (info reposListOptions $ progDesc "List repositories")
+            <> command "remove" (info reposRmOptions   $ progDesc "Remove repository in current directory")
+            <> command "add"    (info reposAddOptions  $ progDesc "Add repository in current directory")
+             )
+
+reposListOptions :: Parser RepoAction
+reposListOptions = pure RList
+
+reposRmOptions :: Parser RepoAction
+reposRmOptions = pure RRemove 
+
+reposAddOptions :: Parser RepoAction
+reposAddOptions = subparser
+                $ command "git"   (info reposGitOptions   $ progDesc "Git repository")
+               <> command "other" (info reposOtherOptions $ progDesc "Arbitrary repository")
+
+reposGitOptions :: Parser RepoAction
+reposGitOptions = RAddGit <$> argument str (metavar "remote")
+
+reposOtherOptions :: Parser RepoAction
+reposOtherOptions = RAddOther 
+                <$> mconcat . intersperse " "
+                <$> many (argument str (metavar "command"))
 
 actOptions :: Parser Action
 actOptions = pure Activate
 
 deactOptions :: Parser Action
 deactOptions = pure Deactivate
-
-helpOptions :: Parser Action
-helpOptions = pure Help
 
 
 
